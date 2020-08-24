4a9f088f03cc192c3b96a4ea0e598c0599bb8720
tgilbert14 <63256691+tgilbert14@users.noreply.github.com>
2020-08-24 16:34:51 -0700

    Update ui.R

--- ui.R
+++ ui.R
@@ -7,10 +7,14 @@ ui <- dashboardPage(skin = 'blue',
       selectInput("Select", "Please type and select your site:",
                   choices = c("SYCA", "ARIK","BARC","BIGC","BLDE","BLUE","BLWA","CARI","COMO","CRAM","CUPE","FLNT","GUIL","HOPB","KING","LECO","LEWI","LIRO","MART","MAYF","MCDI","MCRA","OKSR","POSE","PRIN","PRLA","PRPO","REDB","SUGG","TECR","TOMB","TOOK","WALK","WLOU"), selected = F, multiple = T),
       selectInput("Select_A", "Please select one of these Analytes [External Lab]:",
-                  choices = c('ANC','Br','Ca','Cl','CO3','Conductivity'='conductivity','DIC','DOC','F','Fe','HCO3','K','Mg','Mn','Na','NH4 - N','NO2 - N','NO3+NO2 - N','Ortho - P','pH','Si','SO4','TDN','TDP','TDS','TN','TOC','TP','TPC','TPN','TSS','TSS - Dry Mass','UV Absorbance (250 nm)','UV Absorbance (280 nm)'), selected = F, multiple = F),
+                  choices = c('ANC','Br','Ca','Cl','CO3','Conductivity'='conductivity','DIC','DOC','F','Fe','HCO3','K','Mg','Mn','Na','NH4 - N','NO2 - N','NO3+NO2 - N','Ortho - P','pH','Si','SO4','TDN','TDP','TDS','TN','TOC','TP','TPC','TPN','TSS','TSS - Dry Mass','UV Absorbance (250 nm)','UV Absorbance (280 nm)','Discharge'='Discharge-FIELD'), selected = F, multiple = F),
       selectInput("Select_B", "Please select something to compare it to:",
-                  choices = c('Discharge'='Discharge-FIELD','Domain ANC'='ANC-DOMAIN','Domain ALK'='ALK-DOMAIN','ANC','Br','Ca','Cl','CO3','Conductivity'='conductivity','DIC','DOC','F','Fe','HCO3','K','Mg','Mn','Na','NH4 - N','NO2 - N','NO3+NO2 - N','Ortho - P','pH','Si','SO4','TDN','TDP','TDS','TN','TOC','TP','TPC','TPN','TSS','TSS - Dry Mass','UV Absorbance (250 nm)','UV Absorbance (280 nm)'), selected = F, multiple = F),
+                  choices = c('Discharge'='Discharge-FIELD','ANC','Br','Ca','Cl','CO3','Conductivity'='conductivity','DIC','DOC','F','Fe','HCO3','K','Mg','Mn','Na','NH4 - N','NO2 - N','NO3+NO2 - N','Ortho - P','pH','Si','SO4','TDN','TDP','TDS','TN','TOC','TP','TPC','TPN','TSS','TSS - Dry Mass','UV Absorbance (250 nm)','UV Absorbance (280 nm)'), selected = F, multiple = F),
       submitButton("    Process NEON Site Selection    "),
+    
+    #These inputs dont work.. need if statements in server.R to work
+    ## 'Domain ANC'='ANC-DOMAIN','Domain ALK'='ALK-DOMAIN'
+    
     
       sidebarMenu(
         menuItem('Analyte Comaprison',tabName = 'E',icon=icon('chart-area')),
